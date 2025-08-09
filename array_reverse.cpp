8f4ab705f5c94a97d662afe91926d18169567fd7
Mushfiq Shahriar <mushfiqshahriarms@gmail.com>
2025-08-09 16:32:10 +0600

    Lab 1

---
+++ array_reverse.cpp
@@ -0,0 +1,32 @@
+/*4. Write a program to reverse an Array.*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter The number of array element : ";
+    cin>>n;
+
+    int a[n];
+    cout<<"Enter array elements : ";
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    };
+
+    cout<<"Array before reversing: ";
+    for(int i=0; i<n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+
+    cout<<"Array after reversing: ";
+    for(int i=n-1; i>=0; i--)
+    {
+        cout<<a[i]<<" ";
+    }
+}
