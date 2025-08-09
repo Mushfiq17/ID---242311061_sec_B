b639d133a5b59a97ec24516219cfcb619d3b494f
Mushfiq Shahriar <mushfiqshahriarms@gmail.com>
2025-08-09 16:18:46 +0600

    Lab 1

---
+++ findlargest.cpp
@@ -0,0 +1,27 @@
+/*1. Write a C/C++ program that find the largest number among the three numbers. */
+
+#include<iostream>
+using namespace std;
+void find_largNum(int a, int b, int c)
+{
+    if (a > b && a > c)
+    {
+        cout << a << " is the largest number among three number.";
+    }
+    else if (b > a && b > c)
+    {
+        cout << b << " is the largest number among three number.";
+    }
+    else
+    {
+        cout << c << " is the largest number among three number.";
+    }
+}
+int main()
+{
+    int num1, num2, num3;
+    cout << "Enter three numbers: ";
+    cin >> num1 >> num2 >> num3;
+    find_largNum(num1, num2, num3);
+    return 0;
+}
