9ae96489121b757d0c31579ea63ebc45cb0b414d
Mushfiq Shahriar <mushfiqshahriarms@gmail.com>
2025-08-09 16:25:41 +0600

    Lab 1

---
+++ pyramid.cpp
@@ -0,0 +1,27 @@
+/*3. Write a Program to create a pyramid pattern using C/C++.
+*
+***
+*****
+*******
+*********
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row;
+    cout<<"Enter the number of rows" <<endl;
+    cin>>row;
+    for(int i =1 ; i <= row; i++)
+    {
+        for(int j=1 ; j <= row-i ; j++)
+        {
+            cout<< " ";
+        }
+        for(int j=1 ; j <= 2*i-1; j++)
+        {
+            cout<< "*";
+        }
+        cout<<endl;
+    }
+}
